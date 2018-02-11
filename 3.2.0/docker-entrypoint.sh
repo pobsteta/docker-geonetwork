f92a4d0b1753da2328f9b553f53746a6b67843ed
pobsteta <pascal.obstetar@gmail.com>
2018-02-11 15:00:55 +0100

    maj all files

--- 3.2.0/docker-entrypoint.sh
+++ 3.2.0/docker-entrypoint.sh
@@ -7,6 +7,9 @@ if [ "$1" = 'catalina.sh' ]; then
 
 	#Set geonetwork data dir
 	export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
+	
+	#Fixing the xml API authorization
+	sed -i -e 's#param name="skipInfo" value="y"#param name="skipInfo" value="n"#g' $CATALINA_HOME/webapps/geonetwork/WEB-INF/config/config-service-xml-api.xml
 fi
 
 exec "$@"
